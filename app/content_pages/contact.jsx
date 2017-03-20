67e0e925783728d51161cdb681d5367d15e83af2
Ulrich Sinn <uli@addtothenoise.com>
2017-03-20 15:49:21 -0700

    contact slugged in

--- app/content_pages/contact.jsx
+++ app/content_pages/contact.jsx
@@ -6,15 +6,24 @@ import ContentFooter from '../components/content_footer';
 const Contact = () => {
   return (
     <div id="contact" className="contentFull">
-      <div id="patronsWrapper">
-        <div id="patronsHeader">
+      <div id="contactWrapper">
+        <div id="contactHeader">
           <div id="contactTitle">CONTACT</div>
         </div>
-        <div id="patronsLogos">
-          <a href="http://mcgill.ca" target="_blank"><div id="mc_gill" className="logo"></div></a>
-          <a href="https://www.mbam.qc.ca/en/" target="_blank"><div id="museum_fine_arts" className="logo"></div></a>
-          <a href="http://www.cirmmt.org/" target="_blank"><div id="cirmmt" className="logo"></div></a>
-          <a href="http://lesalon1861.com/" target="_blank"><div id="le_salon" className="logo"></div></a>
+        
+        <div id="contactAddresses">
+          <div className="contactAddress">
+            <p>Dr. Melissa Park</p>
+            <p>addres[at]someurl.com</p>
+          </div>
+          <div className="contactAddress">
+            <p>Dr. Melissa Park</p>
+            <p>addres[at]someurl.com</p>
+          </div>
+          <div className="contactAddress">
+            <p>Dr. Melissa Park</p>
+            <p>addres[at]someurl.com</p>
+          </div>
         </div>
       </div>
       <ContentFooter text='Interfacing Biomusic & Autism © 2017'/>
