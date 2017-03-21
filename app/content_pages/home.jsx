b2be132c7bd56c7ed36498f07677e5eae169de59
Ulrich Sinn <uli@addtothenoise.com>
2017-03-21 15:28:41 -0700

    slide 4 + 5 started

--- app/content_pages/home.jsx
+++ app/content_pages/home.jsx
@@ -4,8 +4,12 @@ import React from 'react';
 import Slide_1 from '../slideshow/slide_1';
 import Slide_2 from '../slideshow/slide_2';
 import Slide_3 from '../slideshow/slide_3';
+import Slide_4 from '../slideshow/slide_4';
+import Slide_5 from '../slideshow/slide_5';
 const Home = () => {
   return <div id="home" className="contentHome">
+    <Slide_5 />
+    <Slide_4 />
     <Slide_3 />
     <Slide_2 />
     <Slide_1 />
---
+++ app/slideshow/slide_4.js
@@ -0,0 +1,35 @@
+/**
+ * Created by ulrichsinn on 03/21/2017.
+ */
+
+import '../styles/main.scss';
+
+import React, {Component} from 'react';
+
+const slide_bg_4 = require('../images/slide_bg_4.png');
+
+export  default class Slide_4 extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return (
+      <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 817 330">
+        <image width="817" height="330" href={slide_bg_4}
+               x={0} y={0}>
+        </image>
+        <g transform="translate(320,110)">
+          {/*           <rect x="0" y="0" width="480" height="105" stroke="#ffffff" strokeWidth="1" fill="none"/>
+         <text className="slide_text_2" fill="#ffffff">
+            <tspan x={20} y={30}>Interfacing Biomusic & Autism blurs distinctions</tspan>
+            <tspan x={20} y={55}>between humans and technologies, emotions and art,</tspan>
+            <tspan x={20} y={80}>public and private experiences.</tspan>
+          </text>*/}
+        </g>
+      </svg>
+    );
+  }
+}
+
+
