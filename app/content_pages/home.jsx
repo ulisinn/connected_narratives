5fb45269b161f1ccd11049c874e0903110de35d8
Ulrich Sinn <uli@addtothenoise.com>
2017-03-21 15:12:27 -0700

    slide 2 + 3 cereated

--- app/content_pages/home.jsx
+++ app/content_pages/home.jsx
@@ -2,8 +2,12 @@ import '../styles/main.scss';
 
 import React from 'react';
 import Slide_1 from '../slideshow/slide_1';
+import Slide_2 from '../slideshow/slide_2';
+import Slide_3 from '../slideshow/slide_3';
 const Home = () => {
   return <div id="home" className="contentHome">
+    <Slide_3 />
+    <Slide_2 />
     <Slide_1 />
   </div>;
 };
---
+++ app/slideshow/slide_2.js
@@ -0,0 +1,35 @@
+/**
+ * Created by ulrichsinn on 03/21/2017.
+ */
+
+import '../styles/main.scss';
+
+import React, {Component} from 'react';
+
+const slide_bg_2 = require('../images/slide_bg_2.png');
+
+export  default class Slide_2 extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return (
+      <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 817 330">
+        <image width="817" height="330" href={slide_bg_2}
+               x={0} y={0}>
+        </image>
+        <g transform="translate(340,118)">
+          <rect x="0" y="0" width="345" height="123" stroke="#ffffff" strokeWidth="1" fill="none"/>
+          <text className="slide_text_2" fill="#ffffff">
+            <tspan x={14} y={30}>Biomusic is an innovative technology</tspan>
+            <tspan x={14} y={55}>that translates the physiological</tspan>
+            <tspan x={14} y={80}>representations of emotional</tspan>
+            <tspan x={14} y={105}>and mental states into musical output</tspan>
+          </text>
+        </g>
+      </svg>
+    );
+  }
+}
+
