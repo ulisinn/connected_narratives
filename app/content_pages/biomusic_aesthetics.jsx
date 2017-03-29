634d6910e8caac4aa8b315e1ebd5030b89593e16
Ulrich Sinn <uli@addtothenoise.com>
2017-03-29 12:53:21 -0700

    copy updated

--- app/content_pages/biomusic_aesthetics.jsx
+++ app/content_pages/biomusic_aesthetics.jsx
@@ -10,19 +10,17 @@ const BiomusicAesthetics = () => {
     <ContentHeader text="(April 24th)"/>
     <div className="contentBody">
       <p>The <span className="contentBodyEm">Sharing the Museum</span> program will host an engaged experience of
-        biomusic in the exhibits and Studios Art & Education Michel de la Chenelière International Atelier of the <span
-          className="helveticaBold contentBodyIt">Musée des Beaux Arts de Montréal </span> <span
-          className="contentBodyEm"><a
-          href="http://www.mbam.qc.ca/education/studio-art-education-michel-de-la-cheneliere" target="_blank">(www.mbam.qc.ca/education/studio-art-education-michel-de-la-cheneliere)</a></span>,
+        biomusic in the exhibits and <span className="contentBodyEm"><a
+          href="http://www.mbam.qc.ca/education/studio-art-education-michel-de-la-cheneliere" target="_blank">Studios Art & Education Michel de la Chenelière International Atelier</a></span>
+        {' '}of the{' '}<span className="helveticaBold contentBodyIt">Musée des Beaux Arts de Montréal{''}</span>,
         our cultural collaborator on the project.</p>
       <p>{'\u00A0'}</p>
-      <p>This event grounds technical and theoretical knowledge exchange in a common, embodied experience.
-        Participants, deliberately grouped to represent intersectoral stakes (e.g.
-        citizen/academic/student, citizen/industry/student) will experience biomusic. Each group will be given a
-        physiological sensor paired with a smartphone which will play their biomusic over headphones and will follow
-        pre-determined paths through the exhibit halls, created to elicit diverse emotional responses. Using a simple
-        program designed by engineering students (ECSE 456) under Blain-Moraes, the physiological sounds of specific
-        individual will be available for participants to “tune in” to on the smartphone.</p>
+      <p>This event will grounds the morning's technical and theoretical knowledge exchange on biomusic in a common,
+        embodied experience. Participants, deliberately grouped to represent intersectoral stakes (e.g.
+        citizen/academic/student, citizen/industry/student) will experience biomusic together. As they move through
+        predetermined sequences of artworks chosen to elicit a range of emotional responses, each group will be able to
+        “tune in” to each other’s biomusic over headphones using a simple program designed by engineering students under
+        Blain-Moraes.</p>
       <p>{'\u00A0'}</p>
       <p>After time to explore the exhibits, participants will be able to observe an improvisatory movement session with
         professional dancer (Lee) in the Atelier. Thus, this component, designed in collaboration with an arts educator
